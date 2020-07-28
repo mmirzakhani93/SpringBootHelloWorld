b653d1b4904b181c62a7673788cc871bf63699d0
Mohammad Mirzakhani <mmirzakhani1993@gmail.com>
2020-07-28 14:30:50 +0430

    first commit

---
+++ src/main/java/ir/whatsoft/demo/HelloContent.java
@@ -0,0 +1,28 @@
+package ir.whatsoft.demo;
+
+public class HelloContent {
+
+    private String name;
+    private String text;
+
+    public HelloContent(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/ir/whatsoft/demo/HelloController.java
@@ -0,0 +1,13 @@
+package ir.whatsoft.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/sayhello")
+    public HelloContent sayHello() {
+        return new HelloContent("WhatSoft", "Hello, WhatSoft!");
+    }
+}
